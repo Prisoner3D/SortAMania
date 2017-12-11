4cc7e4702fb2f51ed95b419e1b6d91ef3af36d2d
Thomas <Thomas@DESKTOP-52CN92E>
2017-12-11 00:30:24 -0500

    Fixed crashes

--- SortAMania/src/Team9SortCompetition.java
+++ SortAMania/src/Team9SortCompetition.java
@@ -4,27 +4,26 @@ public class Team9SortCompetition extends SortCompetition {
 	@Override
 	public int challengeOne(int[] arr)
 	{
-		int size = 10000;
-		int[] count = new int[size];
-		for (int i = 0; i < size; i++)
+		int[] count = new int[10000];
+		for (int i = 0; i < 10000; i++)
 		{
 			count[arr[i]]++;
 		}
-		for (int i = 1; i < size; i++)
+		for (int i = 1; i < 10000; i++)
 		{
 			count[i] = count[i] + count[i-1];
 		}
-		int[] sorted = new int[size];
-		for (int i = 0; i < size; i++)
+		int[] sorted = new int[10000];
+		for (int i = 0; i < 10000; i++)
 		{
 			sorted[count[arr[i]] - 1] = arr[i];
 			count[arr[i]]--;
 		}
-		for (int i = 0; i < size; i++)
+		for (int i = 0; i < 10000; i++)
 		{
 			arr[i] = sorted[i];
 		}
-		return ((arr[size/2])+(arr[(size/2)-1]))/2;
+		return ((arr[5000])+(arr[4999]))/2;
 	}
 
 	@Override
@@ -36,35 +35,34 @@ public class Team9SortCompetition extends SortCompetition {
 	@Override
 	public int challengeThree(int[] arr)
 	{
-		int size = 10000;
-		int[] count = new int[size];
-		for (int i = 0; i < size; i++)
+		int[] count = new int[10000];
+		for (int i = 0; i < 10000; i++)
 		{
 			count[arr[i]]++;
 		}
-		for (int i = 1; i < size; i++)
+		for (int i = 1; i < 10000; i++)
 		{
 			count[i] = count[i] + count[i-1];
 		}
-		int[] sorted = new int[size];
-		for (int i = 0; i < size; i++)
+		int[] sorted = new int[10000];
+		for (int i = 0; i < 10000; i++)
 		{
 			sorted[count[arr[i]] - 1] = arr[i];
 			count[arr[i]]--;
 		}
-		for (int i = 0; i < size; i++)
+		for (int i = 0; i < 10000; i++)
 		{
 			arr[i] = sorted[i];
 		}
-		return ((arr[size/2])+(arr[(size/2)-1]))/2;
+		return ((arr[5000])+(arr[4999]))/2;
 	}
 
 	@Override
 	public int challengeFour(int[][] arr) {
-		int[] medians = new int[1000];
+		int[] medians = new int[arr.length];
 		for (int i = 0; i < arr.length; i++)
 		{
-			medians[i] = challengeOne(arr[i]);
+			medians[i] = countingSort(arr[i]);
 		}
 		for (int i = 0; i < medians.length - 1; i++)  
 		{  
@@ -82,9 +80,10 @@ public class Team9SortCompetition extends SortCompetition {
 	        arr[index] = arr[i];
 	        arr[i] = temp2;
 	    }
+		countingSort(medians);
 		return ((medians[medians.length/2])+(medians[(medians.length/2)-1]))/2;
 	}
-
+	
 	@SuppressWarnings("rawtypes")
 	@Override
 	public int challengeFive(Comparable[] arr, Comparable query) {
@@ -96,41 +95,33 @@ public class Team9SortCompetition extends SortCompetition {
 	public String greeting() {
 		return "Team Numba 9 Here";
 	}
-	private static void quickSort(String[] list1, int front, int back)
+	
+	public static int countingSort(int[] arr)
 	{
-		if (front >= back)
+		int[] count = new int[10000];
+		for (int i = 0; i < 100; i++)
 		{
-			return;
+			count[arr[i]]++;
 		}
-		else
+		for (int i = 1; i < 10000; i++)
 		{
-			int PIVOTindex = partition(list1, front, back);
-			quickSort(list1, front, PIVOTindex);
-			quickSort(list1, PIVOTindex + 1, back);
+			count[i] = count[i] + count[i-1];
 		}
+		int[] sorted = new int[10000];
+		for (int i = 0; i < 100; i++)
+		{
+			sorted[count[arr[i]] - 1] = arr[i];
+			count[arr[i]]--;
+		}
+		for (int i = 0; i < 100; i++)
+		{
+			arr[i] = sorted[i];
+		}
+		return ((arr[50])+(arr[49]))/2;
 	}
 	
-	private static int partition(String[] list, int front, int back)
-	{
-		String pivot = list[front];
-		int i = front;
-		for (int j = front + 1; j < back; j++)
-		{
-			if (list[j].compareTo(pivot) <= 0)
-			{
-				i++;
-				String temp = list[j];
-				list[j] = list[i];
-				list[i] = temp;
-			}
-		}
-		String temp = list[front];
-		list[front] = list[i];
-		list[i] = temp;
-		return i;
-	}
-	
-	private static int binarySearch(String[] a, String key) {
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private static int binarySearch(Comparable[] a, Comparable key) {
 		int min = 0;
 	    int max = a.length -1;
 	    int mid = 0;
@@ -138,42 +129,22 @@ public class Team9SortCompetition extends SortCompetition {
 	    while (min <= max) 
 	    {
 	    	mid = min + (max - min) / 2;
-	    	if (a[mid].compareTo(key) == 0) 
-	    	{
-	    		return mid;
-	    	} 
-	    	else if (a[mid].compareTo(key) > 0) 
+	    	if (a[mid].compareTo(key) < 0)
 	    	{
 	    		min = mid + 1;
-	    	} 
-	    	else 
+	    	}
+	    	else if (a[mid].compareTo(key) > 0)
 	    	{
 	    		max = mid - 1;
 	    	}
-	    }
-	    return -1;
-	}
-	
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	private static int binarySearch(Comparable[] a, Comparable key) {
-		int min = 0;
-		int max = a.length -1;
-		int mid = 0;
-		
-		while (min <= max) {
-			mid = min + (max - min) / 2;
-			if (a[mid].compareTo(key) == 0) 
-			{
-	        return mid;
-			} 
-			else if (a[mid].compareTo(key) > 0) 
-			{
-				min = mid + 1;
-			} 
-			else 
-	      	{
-				max = mid - 1;
-	      	}
+	    	else if (min != mid)
+	    	{
+	    		max = mid; // Keeps searching
+	    	}
+	    	else
+	    	{
+	    		return mid;
+	    	}
 	    }
 	    return -1;
 	}
@@ -193,32 +164,22 @@ public class Team9SortCompetition extends SortCompetition {
     }
 	
 	@SuppressWarnings({ "rawtypes", "unchecked" })
-	private static <T extends Comparable<T>> int partition(Comparable[] a, int front, int back) {
-        int i = front + 1;
-        int j = back;
-
-        while(i <= j) {
-        	if(a[j].compareTo(a[front]) > 0) { 
-                j--;
-            }
-            else if(a[i].compareTo(a[front]) <= 0) { 
-                i++; 
-            }
-            else if (j < i) {
-                break;
-            }
-            else
-            {
-            	swap(a, i, j);
-            }
-        }
-        swap(a, front, j);
-        return j;
+	private static <T extends Comparable<T>> int partition(Comparable[] list, int front, int back) {
+		Comparable pivot = list[front];
+		int i = front;
+		for (int j = front + 1; j < back; j++)
+		{
+			if (list[j].compareTo(pivot) <= 0)
+			{
+				i++;
+				Comparable temp = list[j];
+				list[j] = list[i];
+				list[i] = temp;
+			}
+		}
+		Comparable temp = list[front];
+		list[front] = list[i];
+		list[i] = temp;
+		return i;
     }
-	
-	private static void swap(Object[] a, int i, int j) {
-		Object temp = a[i];
-		a[i] = a[j];
-		a[j] = temp;
-	}
 }
